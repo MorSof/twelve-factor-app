22aa71998898405688c579419e68ffb30ceb4fc1
Mor Soferian <47902941+MorSof@users.noreply.github.com>
2020-12-31 14:48:13 +0200

    * First Commit * * Create an express app with Hello Word endpoint * Add test to endpoint with Mocha

---
+++ test/test.js
@@ -0,0 +1,16 @@
+const bodyParser = require('body-parser');
+const request = require("supertest");
+const router = require("../routes/index");
+const express = require("express");
+
+const app = express(); //an instance of an express app, a 'fake' express app
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use("/", router); //routes
+
+describe("GET /", () => {
+    it("respond with Hello World", (done) => {
+        request(app).get("/").expect("Hello World", done);
+    })
+});
