c2b65b69571da113d7c58c55aa1384c62297dc25
Mor Soferian <47902941+MorSof@users.noreply.github.com>
2020-12-31 17:38:36 +0200

    * Test CI/CD Commit2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  return res.send("Hello World via github!");
+  return res.send("Hello World");
 });
 
 module.exports = router;
