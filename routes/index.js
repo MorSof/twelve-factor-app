38f0bbb8a282a9fe25146728a7cfb88b81abb04c
Mor Soferian <47902941+MorSof@users.noreply.github.com>
2021-01-02 10:42:08 +0200

    * Test CI/CD Commit13

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  return res.send("Hello");
+  return res.send("Hello World");
 });
 
 /* GET home page. */
 router.get('/test', function(req, res, next) {
-  return res.send("Sruli!!!!!");
+  return res.send("Hodi!!!!!");
 });
 
 module.exports = router;
