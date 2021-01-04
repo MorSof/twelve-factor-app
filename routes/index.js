649b138e195cfed5d6b1565a7e7c7c8d7ee0dd63
Yosi Bukris <yosi@playtium.com>
2021-01-04 14:03:12 +0200

    add comments

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,17 @@ router.get('/', function(req, res, next) {
   return res.send("Hello World From Afeka College yay!");
 });
 
+/* GET temp files. */
 router.get('/tempFiles', async function(req, res, next) {
   await tempFilesService.getTempFiles(req, res);
 });
 
+/* POST new temp file. */
 router.post('/', async function(req, res, next) {
   await tempFilesService.addNewTempFile(req, res);
 });
 
+/* DELETE all temp files. */
 router.delete('/', async (req, res) => {
   await tempFilesService.deleteAllFiles(req, res);
 })
