5a60a4f08aa14afbea78668321f4cbed6e191f2b
Mor Soferian <47902941+MorSof@users.noreply.github.com>
2021-01-05 19:12:11 +0200

    * connect to new postgres

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,14 @@
-const tempFilesService = require('../services/tempService');
 var express = require('express');
 var router = express.Router();
 const pool = require("../db/postgres");
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  return res.send("Hello World From Afeka College yay!!");
+  return res.send("Hello World From Afeka College yay!!!");
 });
-
-/* GET temp files. */
-// router.get('/tempFiles', async function(req, res, next) {
-//   await tempFilesService.getTempFiles(req, res);
-// });
 
 /* POST new temp file. */
 router.post('/', async (req, res, next) => {
-  // await tempFilesService.addNewTempFile(req, res);
   try {
     const db_res = (await pool.query(
         "INSERT INTO messages(name, message)\n" +
@@ -27,13 +19,6 @@ router.post('/', async (req, res, next) => {
     console.error(err.message);
     res.json(err.message);
   }
-
-
 });
-
-/* DELETE all temp files. */
-// router.delete('/', async (req, res) => {
-//   await tempFilesService.deleteAllFiles(req, res);
-// })
 
 module.exports = router;
