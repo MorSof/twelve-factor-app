2f8bfdea1c37f53d8c69617a63cfbb5fbb786847
Mor Soferian <47902941+MorSof@users.noreply.github.com>
2021-01-05 16:03:56 +0200

    * connect to new mongo cluster7

--- db/temp.js
+++ db/temp.js
@@ -4,10 +4,6 @@ const config = require('config');
 const tempSchema = new mongoose.Schema({
     name: String,
     message: String
-}, {
-    capped: { size: 1024 },
-    bufferCommands: false,
-    autoCreate: false // disable `autoCreate` since `bufferCommands` is false
 });
 
 const TempFileData = new mongoose.model('messages', tempSchema);
