c1cbe46be9a6c7065c3bf25a0c1bc798449ab5e6
Mor Soferian <47902941+MorSof@users.noreply.github.com>
2021-01-05 19:06:06 +0200

    * connect to new postgres

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const tempFilesService = require('../services/tempService');
 var express = require('express');
 var router = express.Router();
+const pool = require("../db/postgres");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,18 +10,30 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET temp files. */
-router.get('/tempFiles', async function(req, res, next) {
-  await tempFilesService.getTempFiles(req, res);
-});
+// router.get('/tempFiles', async function(req, res, next) {
+//   await tempFilesService.getTempFiles(req, res);
+// });
 
 /* POST new temp file. */
 router.post('/', async (req, res, next) => {
-  await tempFilesService.addNewTempFile(req, res);
+  // await tempFilesService.addNewTempFile(req, res);
+  try {
+    const db_res = (await pool.query(
+        "INSERT INTO messages(name, message)\n" +
+        "VALUES ($1, $2)\n" +
+        "RETURNING *;\n", [req.body.name, req.body.message])).rows[0];
+    res.json(db_res);
+  }catch(err){
+    console.error(err.message);
+    res.json(err.message);
+  }
+
+
 });
 
 /* DELETE all temp files. */
-router.delete('/', async (req, res) => {
-  await tempFilesService.deleteAllFiles(req, res);
-})
+// router.delete('/', async (req, res) => {
+//   await tempFilesService.deleteAllFiles(req, res);
+// })
 
 module.exports = router;
