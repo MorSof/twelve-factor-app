5b02e28be98bf3b5e6242294f5a005cdcde95cc6
Mor Soferian <47902941+MorSof@users.noreply.github.com>
2021-01-05 19:59:37 +0200

    * connect to new postgres3

--- db/postgres.js
+++ db/postgres.js
@@ -5,7 +5,7 @@ const pool = new Pool({
     user: "postgres",
     password: "jCzMzrb0EPd0hYchFB96KwtCZkwlJvexcK3W5Qni",
     database: "cloud-computing",
-    host: "cloud-computing-300816:us-central1:myinstance",
+    host: "45.55.46.246",
     port: 5432
 })
 
